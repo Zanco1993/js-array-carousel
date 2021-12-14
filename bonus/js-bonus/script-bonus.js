febc10af41889ae13e7a5a0d1c1cdfbd9bf71882
Zanco1993 <zanconatofabio@gmail.com>
2021-12-14 17:39:51 +0100

    concluso l'esercizio bonus con l'aggiunta di una immagine

--- bonus/js-bonus/script-bonus.js
+++ bonus/js-bonus/script-bonus.js
@@ -2,14 +2,19 @@
 
 
 const listTitle = [
-    'QUESTO E\' IL PRIMO TITOLO',
-    'QUESTO E\' IL SECONDO TITOLO',
-    'QUESTO E\' IL TERZO TITOLO'
+    'QUESTO E\' IL PRIMO ALIMENTO',
+    'QUESTO E\' IL SECONDO ALIMENTO',
+    'QUESTO E\' IL TERZO ALIMENTO'
 ];
 const listContent = [
-    'Questo testo fa riferimento al PRIMO paragrafo',
-    'Questo testo fa riferimento al SECONDO paragrafo',
-    'Questo testo fa riferimento al TERZO paragrafo',
+    'Questo testo fa riferimento al Burro di Arachidi',
+    'Questo testo fa riferimento alla Farina di Avena',
+    'Questo testo fa riferimento alle Proteine in Polvere',
+];
+const listFood = [
+    './img/burro.jpg',
+    './img/avena.jpg',
+    './img/proteine.png'
 ];
 
 let currentIndex = 0;
@@ -18,12 +23,14 @@ let currentIndex = 0;
 //dichiaro le costanti
 const titleElement = document.querySelector(".content-box .title");
 const paragraphElement = document.querySelector(".content-box .content-paragraph");
+const imageElement = document.querySelector(".news .content-image img");
 const buttonPrevious = document.querySelector(".button-previous .fas.fa-arrow-left");
 const buttonNext = document.querySelector(".button-next .fas.fa-arrow-right");
 
 //scrivi nell'html l'argomento 0 dell'array
 titleElement.innerHTML = listTitle[currentIndex];
 paragraphElement.innerHTML = listContent[currentIndex];
+imageElement.src = listFood[currentIndex];
 
 //se clicco la freccia avanti, incrementa di 1 l'indice e passa a quello successivo
 //quando arriva alla fine degli elementi, torna al prima argomento della lista
@@ -34,6 +41,7 @@ buttonNext.addEventListener('click', function(){
     }
     titleElement.innerHTML = listTitle[currentIndex];
     paragraphElement.innerHTML = listContent[currentIndex];
+    imageElement.src = listFood[currentIndex];
 })
 
 //se clicco freccia indietro, decrementa di 1 l'indice
@@ -47,4 +55,5 @@ buttonPrevious.addEventListener('click', function(){
     }
     titleElement.innerHTML = listTitle[currentIndex];
     paragraphElement.innerHTML = listContent[currentIndex];
+    imageElement.src = listFood[currentIndex];
 })
